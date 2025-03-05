124ab27004227ac83089d2349f90cae2500335c8
Emilio Orozco <eaorozco3@miners.utep.edu>
2025-03-05 00:13:18 -0700

    Sign in, login, and logout implemented successfully.

--- src/auth.js
+++ src/auth.js
@@ -1,1 +1,45 @@
-import { auth } from "./index.js";
+import { auth } from "./firebase.js";
+import { db } from "./firebase.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+
+export async function signUp(firstName, lastName, email, password) {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+        console.log("User signed up with the following email:", userCredential.user.email)
+        console.log("User was given the following id:", userCredential.user.uid)
+
+        const userRef = doc(db, "Users", userCredential.user.uid)
+        await setDoc(userRef, {
+            FirstName: firstName,
+            LastName: lastName,
+            Timestamp: new Date()
+        })
+    }
+
+    catch(Error) {
+        console.log(Error)
+    }
+}
+
+export async function logIn(email, password) {
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password)
+        window.location.href = "songmanager.html"
+    } 
+    
+    catch (error) {
+        console.log(Error)    
+    }
+}
+
+export async function logOut() {
+    try {
+        await signOut(auth)
+        console.log("User logged out.")
+    } 
+    
+    catch(Error) {
+        console.log(Error)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-async function getSongs() {
+import { logIn, signUp, logOut } from "./auth"
+
+const signupForm = document.querySelector("#signUpForm")
+signupForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const firstname = document.getElementById("firstName").value
+    const lastname = document.getElementById("lastName").value
+    const email = document.getElementById("signupEmail").value
+    const password = document.getElementById("signupPassword").value
+
+    signUp(firstname, lastname, email, password)
+})
+
+const loginForm = document.querySelector("#loginForm")
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const email = document.getElementById("loginEmail").value
+    const password = document.getElementById("loginPassword").value
+
+    logIn(email, password)
+})
+
+const logoutForm = document.querySelector("#logoutForm")
+logoutForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    logOut()
+})
+
+
+/*async function getSongs() {
     try {
         const songsCol = collection(db, "Songs")
         const snapshot = await getDocs(songsCol)
@@ -122,3 +151,4 @@ updateRating.addEventListener("submit", (event) => {
 })
 
 getSongs()
+*/
