7dd5fafbc0df7e4dd0673e04337298e805b88669
Emilio Orozco <eaorozco3@miners.utep.edu>
2025-03-06 19:46:11 -0700

    User is now redirected when creating account.

--- src/auth.js
+++ src/auth.js
@@ -1,69 +1,78 @@
 import { auth } from "./firebase.js";
 import { db } from "./firebase.js";
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { collection, doc, getDocs, setDoc } from "firebase/firestore";
 
 async function fetchUserData(userID) {
-    try {
-       const userDoc = await getDocs(collection(db, "Users"))
-       const userData = userDoc.docs.find(doc => doc.id === userID)?.data()
-       console.log("User data ", userData)
-       document.getElementById("introGreeting").innerHTML = "<h1> Welcome " + userData.FirstName + " to Song Connosseiur!</h1>"
-    } 
-    
-    catch (error) {
-        console.log(Error)    
-    }
+  try {
+    const userDoc = await getDocs(collection(db, "Users"));
+    const userData = userDoc.docs.find((doc) => doc.id === userID)?.data();
+    console.log("User data ", userData);
+    document.getElementById("introGreeting").innerHTML =
+      "<h1> Welcome " + userData.FirstName + " to Song Connosseiur!</h1>";
+  } catch (error) {
+    console.log(Error);
+  }
 }
 
-onAuthStateChanged(auth, async(user) => {
-    if(user) {
-        console.log("Logged in user: ", user.email)
-        await fetchUserData(user.uid)
-    }
-
-    else {
-        console.log("No user signed in.")
-    }
-})
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    console.log("Logged in user: ", user.email);
+    await fetchUserData(user.uid);
+  } else {
+    console.log("No user signed in.");
+  }
+});
 
 export async function signUp(firstName, lastName, email, password) {
-    try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-        console.log("User signed up with the following email:", userCredential.user.email)
-        console.log("User was given the following id:", userCredential.user.uid)
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(
+      "User signed up with the following email:",
+      userCredential.user.email
+    );
+    console.log("User was given the following id:", userCredential.user.uid);
 
-        const userRef = doc(db, "Users", userCredential.user.uid)
-        await setDoc(userRef, {
-            FirstName: firstName,
-            LastName: lastName,
-            Timestamp: new Date()
-        })
-    }
+    const userRef = doc(db, "Users", userCredential.user.uid);
+    await setDoc(userRef, {
+      FirstName: firstName,
+      LastName: lastName,
+      Timestamp: new Date(),
+    });
 
-    catch(Error) {
-        console.log(Error)
-    }
+    window.location.href = "songmanager.html";
+  } catch (Error) {
+    console.log(Error);
+  }
 }
 
 export async function logIn(email, password) {
-    try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password)
-        window.location.href = "songmanager.html"
-    } 
-    
-    catch (error) {
-        console.log(Error)    
-    }
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    window.location.href = "songmanager.html";
+  } catch (error) {
+    console.log(Error);
+  }
 }
 
 export async function logOut() {
-    try {
-        await signOut(auth)
-        console.log("User logged out.")
-    } 
-    
-    catch(Error) {
-        console.log(Error)
-    }
+  try {
+    await signOut(auth);
+    console.log("User logged out.");
+  } catch (Error) {
+    console.log(Error);
+  }
 }
